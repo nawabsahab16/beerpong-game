8a1868af8722d1242abaa4b0cdb8ac0972b0bf9c
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-02-23 21:35:33 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var reshoot = document.getElementById("reshoot");
 var ball = document.getElementById("ball");
 var root = document.documentElement;
 var cupsOut = [];
+
+
+function shootY() {
+    let top = window.getComputedStyle(ball).getPropertyValue("top");
+    ball.classList.remove("shootY");
+    ball.classList.add("shootX");
+    ball.style.top= top;
+    let onclick = "shootX(' ".concat(top.toString()," ')");
+    html.setAttribute("onclick", onclick);  
+}
