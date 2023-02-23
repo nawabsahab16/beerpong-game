3fe749faacec35d97d6530fb6d8f8cf721b76cb3
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-02-23 21:31:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var html = document.getElementById("html");
+var reshoot = document.getElementById("reshoot");
+var ball = document.getElementById("ball");
+var root = document.documentElement;
+var cupsOut = [];
